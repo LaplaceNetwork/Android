8ab3118509d82bd13eaadd7685c444e1b991bb38
raytheon_chen <784780299@qq.com>
2018-10-10 18:01:30 +0800

    code optimizing

--- app/src/main/java/com/orangechain/laplace/activity/BackupsIdentityActionActivity.java
+++ app/src/main/java/com/orangechain/laplace/activity/BackupsIdentityActionActivity.java
@@ -6,16 +6,22 @@ import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.Button;
 
 import com.orangechain.laplace.R;
 import com.orangechain.laplace.base.BaseActivity;
+import com.orangechain.laplace.base.BaseActivityCollector;
 
 public class BackupsIdentityActionActivity extends BaseActivity {
 
+    LocalBroadcastManager localBroadcastManager;
+
     @Override
     public void initWithView() {
+
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
 
         setBottomNavigationViewVisible(View.GONE);
 
@@ -35,9 +41,11 @@ public class BackupsIdentityActionActivity extends BaseActivity {
         exportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                //进入首页
-
+                //进入主界面
+                BaseActivityCollector.finishAndGoBackHistoryActivity(IndexActivity.class);
+                //显示主界面 隐藏index界面
+                Intent intent = new Intent("com.orangechain.laplace.IndexActivity.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);
             }
         });
 
