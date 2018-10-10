f27581eea7daa86b618c2cc4540944bc89e45029
raytheon_chen <784780299@qq.com>
2018-10-10 14:33:01 +0800

    Complete layout about BackupsQRCodeActivity

--- app/src/main/java/com/orangechain/laplace/activity/BackupsQRCodeActivity.java
+++ app/src/main/java/com/orangechain/laplace/activity/BackupsQRCodeActivity.java
@@ -2,12 +2,17 @@ package com.orangechain.laplace.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.orangechain.laplace.R;
 import com.orangechain.laplace.base.BaseActivity;
+import com.orangechain.laplace.base.BaseActivityCollector;
 
 public class BackupsQRCodeActivity extends BaseActivity {
 
@@ -21,6 +26,22 @@ public class BackupsQRCodeActivity extends BaseActivity {
         setToolBarLeftColor(R.color.colorWhite);
 
         setBottomNavigationViewVisible(View.GONE);
+
+
+        //修改图片的填充颜色
+        Drawable imageViewDraw = ContextCompat.getDrawable(this, R.drawable.baseline_report_black_24dp);
+        imageViewDraw.setColorFilter(getResources().getColor(R.color.colorGray), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(imageViewDraw);
+
+        Button saveButton = findViewById(R.id.QRCode_save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //进入主界面
+                BaseActivityCollector.finishAllActivity();
+            }
+        });
 
     }
 
