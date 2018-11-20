11fa76158ced43686d1acb9527313ba72f554afb
raytheon_chen <784780299@qq.com>
2018-11-20 18:07:17 +0800

    add StatusBarSetting

--- app/src/main/java/com/orangechain/laplace/base/BaseActivity.java
+++ app/src/main/java/com/orangechain/laplace/base/BaseActivity.java
@@ -20,6 +20,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,6 +32,7 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 import com.orangechain.laplace.R;
 import com.orangechain.laplace.ToolUtil.SizeConvertUtil;
+import com.orangechain.laplace.ToolUtil.statusBarUtil.StatusBarSetting;
 import com.orangechain.laplace.activity.IndexActivity;
 import com.orangechain.laplace.base.basehelper.MyApplication;
 import com.orangechain.laplace.base.laplaceBaseView.laplaceToolbar;
@@ -505,34 +507,35 @@ public abstract class BaseActivity extends AppCompatActivity {
      */
     public void setStatusBarColor(Activity activity, int statusColor) {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        StatusBarSetting.setStatusBarColor(activity,statusColor);
 
-            Window window = activity.getWindow();
-            //取消状态栏透明
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //添加Flag把状态栏设为可绘制模式
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            //设置状态栏颜色
-            window.setStatusBarColor(statusColor);
-            //设置系统状态栏处于可见状态
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-            //让view不根据系统窗口来调整自己的布局
-            ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
-            View mChildView = mContentView.getChildAt(0);
-            if (mChildView != null) {
-                ViewCompat.setFitsSystemWindows(mChildView, false);
-                ViewCompat.requestApplyInsets(mChildView);
-            }
-
-        } else {
-
-
-
-
-        }
-
+////                setStatusBarColor
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//
+//            Window window = activity.getWindow();
+//            //取消状态栏透明
+//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//            //添加Flag把状态栏设为可绘制模式
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            //设置状态栏颜色
+//            window.setStatusBarColor(statusColor);
+//            //设置系统状态栏处于可见状态
+//            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+//            //让view不根据系统窗口来调整自己的布局
+//            ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
+//            View mChildView = mContentView.getChildAt(0);
+//            if (mChildView != null) {
+//                ViewCompat.setFitsSystemWindows(mChildView, false);
+//                ViewCompat.requestApplyInsets(mChildView);
+//            }
+//
+//        } else {
+//
+//
+//
+//
+//        }
     }
-    
-
 
 }
